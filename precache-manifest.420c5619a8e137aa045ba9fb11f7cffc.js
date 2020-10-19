b3dc40a91eb920bea8191806e580255adfaf16e5
Rufi512 <Rafajos9@gmail.com>
2020-10-18 20:53:49 -0430

    Updates

---
+++ precache-manifest.420c5619a8e137aa045ba9fb11f7cffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4faeff72775b4d0dc610dbd4c123bf",
+    "url": "/Store-Images-React-Redux/index.html"
+  },
+  {
+    "revision": "68b104c27c2118bf7e30",
+    "url": "/Store-Images-React-Redux/static/css/main.5a5d5e1f.chunk.css"
+  },
+  {
+    "revision": "16201f9dc27d3007cf01",
+    "url": "/Store-Images-React-Redux/static/js/2.8f3fc2bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Store-Images-React-Redux/static/js/2.8f3fc2bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b104c27c2118bf7e30",
+    "url": "/Store-Images-React-Redux/static/js/main.2581012e.chunk.js"
+  },
+  {
+    "revision": "94ef1369e594fdc0b133",
+    "url": "/Store-Images-React-Redux/static/js/runtime-main.6f451efa.js"
+  }
+]);
